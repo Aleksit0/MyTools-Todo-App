26875c092b394538da91e31911d98c4519f45db7
Aleksit0 <50078646+Aleksit0@users.noreply.github.com>
2021-08-23 00:20:43 +0200

    read csv

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import accuracy_score 
+from sklearn.utils import shuffle
+
+'''
+  NOTES:
+  B - harmless
+  M - cancerous
+'''
+
+# FEATURE SELECTION
+def remove_correlated_features(X):
+  pass
+
+def remove_features(X, Y):
+  pass
+
+# MODEL TRAINING
+def compute_cost(W, X, Y):
+  pass
+
+def calculate_cost_gradient(W, X_batch, Y_batch):
+  pass
+
+def sgd(features, outputs):
+  pass
+
+def init():
+  data = pd.read_csv('./data.csv')
+
+  # CONVERT TO NUMERICAL VALUES
+  diagnosis_map = {'M' : 1, 'B' : -1}
+  data['diagnosis'] = data['diagnosis'].map(diagnosis_map)
+  
+  print(data)
+
+init()
